6e1d3db52060db118947d09477843188cb494793
Niklas Griessbaum <griessbaum@gmail.com>
2018-07-20 12:29:42 -0700

    removed adds

--- app/src/main/java/org/marco45/polarheartmonitor/MainActivity.java
+++ app/src/main/java/org/marco45/polarheartmonitor/MainActivity.java
@@ -10,8 +10,6 @@ import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.SimpleXYSeries;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -68,15 +66,6 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
         setContentView(R.layout.activity_main);
         Log.i("Main Activity", "Starting Polar HR monitor main activity");
         DataHandler.getInstance().addObserver(this);
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder().build();
-
-        // Start loading the ad in the background.
-        mAdView.loadAd(adRequest);
 
         //Verify if device is to old for BTLE
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
@@ -116,8 +105,6 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
                     listBT();
                 }
             }
-
-
 
             // Create Graph
             plot = (XYPlot) findViewById(R.id.dynamicPlot);
